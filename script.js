21c5fbcca5ea5e8281ed7237dac5c4fc80999bc4
Hamza Sallam <hsallam278@gmail.com>
2026-09-20 00:06:56 +0300

    Make grid work properly after choosing size

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const grid = document.querySelector('#grid');
-const gridSize = 16;
+const gridButton = document.querySelector('#gridButton');
 
+createGrid(16);
 
-for (let i = 0; i < gridSize*gridSize; i++) {
-    const square = document.createElement('div');
-    square.style.width = `${500 / gridSize}px`;
-    square.style.height = `${500 / gridSize}px`;
-    grid.appendChild(square);
+gridButton.addEventListener('click', function(){
+    let gridSize = parseInt(prompt("Enter the grid size (1-100):"));
+    while (gridSize < 1 || gridSize > 100) {
+        gridSize = parseInt(prompt("Please enter a number between 1 and 100:"));
+    }
+
+    grid.innerHTML = "";
+    createGrid(gridSize);
+})
+
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize*gridSize; i++) {
+        const square = document.createElement('div');
+        square.style.width = `${500 / gridSize}px`;
+        square.style.height = `${500 / gridSize}px`;
+        grid.appendChild(square);
+    }
 }
